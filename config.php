5872fb80502a999f3eba1c52210985f9c23373ab
Lucas Craveiro Paes <lucascraveiropaes123@gmail.com>
2017-09-12 14:05:44 -0300

    Arquivos iniciais

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	define('DB_USER','root');
+   	define('DB_SENHA','');
+   	define('DB_HOST','localhost');
+   	define('DB_NOME','framework');
+
+	if ( !defined('HOME_URL') )
+		define('ABSPATH', dirname(__FILE__) . '/');
